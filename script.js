80f6826410c3709e9a8f10a944f950b971a1ab5c
Andrea Desiderio <andreadesideriopro@gmail.com>
2019-05-09 13:22:12 -0700

    spillitn up the javascript files

--- script.js
+++ script.js
@@ -1,50 +1,47 @@
-// window.addEventListener("load", showDate);
+'use strict'
 
-// function showDate() {
-//     const homeDate = document.getElementById("homeDate");
-//     homeDate.innerHTML = new Date().toDateString();
-// }
+window.addEventListener('load', displayClock);
 
-// const timeInterval = setInterval(showTime, 1000);
+function showDiallines (){
+    let diallines = document.getElementsByClassName("diallines");
+    let analogClock = document.getElementsByClassName("analogClock")[0];
+    
+    for (let i = 0; i < 60; i++){
+        analogClock.innerHTML += "<div class='diallines'></div>";
+        diallines[i].style.transform = "rotate(" + 6 * i + "deg)";
+    }
+}
 
-// function showTime(){
-//     const homeClock = document.getElementById("homeClock");
-//     let date = new Date();
-//     homeClock.innerHTML = date.toLocaleTimeString();
-// }
-
-let diallines = document.getElementsByClassName("diallines");
-let analogClock = document.getElementsByClassName("analogClock")[0];
-
-for (let i = 0; i < 60; i++){
-    analogClock.innerHTML += "<div class='diallines'></div>";
-    diallines[i].style.transform = "rotate(" + 6 * i + "deg)";
+function displayClock(){
+    showDiallines();
+    // showDateAndDay();
+   
 }
 
 function runClock(){
-    let weekdays = new Array(7),
-    date = new Date(),
-    hour = date.getHours(),
-    minute = date.getMinutes(),
-    second = date.getSeconds(),
-    currentDate = date.getDate(),
-    month = date.getMonth() + 1,
-    year = date.getFullYear(),
+    let weekdays = new Array(7);
+    let date = new Date();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+    let currentDate = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
     //this is to get 360deg rotation... hour(12hours)*30=360
-    hDeg = (hour * 30) + (minute / 2),
+    let hDeg = (hour * 30) + (minute / 2);
     // minute goes to 60 and 60*6 gets a 360 rotaion
-    mDeg = minute * 6,
-    sDeg = second * 6,
+    let mDeg = minute * 6;
+    let sDeg = second * 6;
 
-    hourHand = document.querySelector(".hourHand"),
-    minuteHand = document.querySelector(".minuteHand"),
-    secondHand = document.querySelector(".secondHand"),
-    analogDate = document.querySelector(".analogClockDate"),
-    analogDay = document.querySelector(".analogClockDay");
+    let hourHand = document.querySelector(".hourHand");
+    let minuteHand = document.querySelector(".minuteHand");
+    let secondHand = document.querySelector(".secondHand");
+    let clockDate = document.querySelector(".clockDate");
+    let clockDay = document.querySelector(".clockDay");
 
-    hourHand.style.transform = "rotate(" + hDeg + "deg)";
-    minuteHand.style.transform = "rotate(" + mDeg + "deg)";
-    secondHand.style.transform = "rotate(" + sDeg + "deg)";
+     hourHand.style.transform = "rotate(" + hDeg + "deg)";
+     minuteHand.style.transform = "rotate(" + mDeg + "deg)";
+     secondHand.style.transform = "rotate(" + sDeg + "deg)";
 
     weekdays[0] = "Sunday";
     weekdays[1] = "Monday";
@@ -56,8 +53,8 @@ function runClock(){
    
    let day = weekdays[date.getDay()];
 
-    analogDate.innerHTML = currentDate + " / " + month +  " / "  + year ;
-    analogDay.innerHTML = day; 
+    clockDate.innerHTML = currentDate + " / " + month +  " / "  + year ;
+    clockDay.innerHTML = day; 
 }
 
 setInterval("runClock()", 1000);

85c63e5eb4b586a91a2a8822e77941d80c733f0b
Andrea Desiderio <andreadesideriopro@gmail.com>
2019-06-08 15:50:44 -0700

    runTimer

--- countDownTimer/countDownTimer.js
+++ countDownTimer/countDownTimer.js
@@ -9,7 +9,7 @@ function showDiallines (){
     }
 }
 
-let calendar = document.getElementById("calendar");
+const calendar = document.getElementById("calendar");
 
 function setCalendar() {
     let now = new Date();
@@ -27,18 +27,14 @@ function preSetTimer(){
 }
 window.addEventListener("load", preSetTimer);
 
-// function showTimer(){
-//   let timerValue = document.getElementById("timerValueContainer").style = "visibility: visible";
-  
-// }
-//  let activateButton = document.getElementById("submit");
-//  activateButton.addEventListener('click', function(){document.getElementById("timerValueContainer").style = "visibility: visible"}); 
-let starButton = document.getElementById("submit");
-starButton.addEventListener('click', calculateTimer);  
-let timer; 
+const startButton = document.getElementById("submit");
+startButton.addEventListener('click', calculateTimer);  
+
+let timer;
+
 function calculateTimer(){
-  alert('hi');
-    //clearInterval(timer);
+  alert('calculateTimer');
+    clearInterval(timer);
     let timerDate = calendar.value.toString().split("-");
     let timerYear = timerDate[0];
     let timerMonth = timerDate[1] - 1;
@@ -47,9 +43,9 @@ function calculateTimer(){
     let timerMinute = Number(document.getElementById("minuteInput").value);
     let timerSecond = Number(document.getElementById("secondInput").value);
   
-     if(timerHour == ""){timerHour = 0};
-     if(timerMinute == ""){timerMinute = 0};
-     if(timerSecond == ""){timerSecond = 0};
+     if(timerHour == ""){timerHour = 0}
+     if(timerMinute == ""){timerMinute = 0}
+     if(timerSecond == ""){timerSecond = 0}
     
     let currentHour = new Date().getHours();
     timerHour = Number(currentHour + timerHour);
@@ -59,44 +55,41 @@ function calculateTimer(){
 
     let currentSecond = new Date().getSeconds();
     timerSecond = Number(currentSecond + timerSecond); 
-  function runTimer (){
-       alert('hey');
+     
     let deadline = new Date(timerYear, timerMonth, timerDay, timerHour, timerMinute, timerSecond);
-    let second = 1000; //one second equal to 1000 milliseconds///
-    let minute = second * 60; ///one minute equal to 60 seconds///
-    let hour = minute * 60; //one hour equal to 60 minutes 
-    let day = hour * 24;   //one day equat to 24 hours //
+    let second = 1000; 
+    let minute = second * 60; 
+    let hour = minute * 60; 
+    let day = hour * 24;   
+    alert (deadline);
 
-      let startTime = new Date();
-      let remain = deadline - startTime;
-      let days = Math.round(remain / day);
-      let hours = Math.round((remain % day) / hour);
-      let minutes = Math.round((remain % hour) / minute);
-      let seconds = Math.ceil((remain % minute) / second);  
-     document.getElementById("timerValue").innerHTML = days + "Days" + hours + "Hrs" + minutes + "Min" + seconds + "Sec";
-        let hDeg = (hours * 30) + (minutes / 2);
-        let mDeg = minutes * 6;
-        let sDeg = seconds * 6;
-        let secondHand = document.querySelector(".secondHand");
-        secondHand.style.transform = "rotate( " + sDeg + "deg)";
+    let runTimer = function(){
+      alert('runtimer');
+      // let startTime = new Date();
+      // let remain = deadline - startTime;
+      // let days = Math.round(remain / day);
+      // let hours = Math.round((remain % day) / hour);
+      // let minutes = Math.round((remain % hour) / minute);
+      // let seconds = Math.round((remain % minute) / second);  
+      //  document.getElementById("timerValue").innerHTML = days + "Days" + hours + "Hrs" + minutes + "Min" + seconds + "Sec";
 
-        let background = document.getElementById("background");
-        background.style.width =  `${100000 / remain}%`;
-        background.style.height =  `${100000 / remain}%`;
+        // let hDeg = (hours * 30) + (minutes / 2);
+        // let mDeg = minutes * 6;
+        // let sDeg = seconds * 6;
+        // let secondHand = document.querySelector(".secondHand");
+        // secondHand.style.transform = "rotate( " + sDeg + "deg)";
+
+        // let background = document.getElementById("background");
+        // background.style.width =  `${100000 / remain}%`;
+        // background.style.height =  `${100000 / remain}%`;
         
-        if (remain <= 0){
-          clearInterval(timer);
-          document.getElementById("timerValue").innerHTML = "Time's Up!";
-          return;
-        }
+        // if (remain < 0){
+        //   clearInterval(timer);
+        //   document.getElementById("timerValue").innerHTML = "Time's Up!";
+        //   return;
+        // }
      }
-     timer = setInterval(runTimer, 1000); 
+     alert('calculAtegain');
+      timer = setInterval(runTimer, 1000); 
      }
-
-
-
- 
-       
-             
-    
     
